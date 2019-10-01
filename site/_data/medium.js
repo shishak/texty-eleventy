b66ef983d184087491007ca09b02aefe1f927353
Shishak <semenshishak@gmail.com>
2019-10-01 03:39:59 +0300

    Update medium.js

--- site/_data/medium.js
+++ site/_data/medium.js
@@ -1,7 +1,7 @@
 var axios  = require('axios');
 var toJSON = require('xml2js').parseString;
 
-var url = process.env.MEDIUM_FEED ||'https://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss';
+var url = process.env.MEDIUM_FEED ||'http://3.120.187.86.xip.io/feed';
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
